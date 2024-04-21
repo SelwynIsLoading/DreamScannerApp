486c62614ed2b13c35d17d4d891a3f457786b5ce
selwyn caubalejo <caubalejoselwyn@gmail.com>
2024-04-21 13:08:55 +0800

    Update

--- DreamScannerApp/obj/Release/net7.0-windows/DreamScannerApp.AssemblyInfo.cs
+++ DreamScannerApp/obj/Release/net7.0-windows/DreamScannerApp.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DreamScannerApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+deadb1c755964f7bab99264a40c04f1c47044ba8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dac3a248eaadc5a1bcd17c7d2a6ef578debcde72")]
 [assembly: System.Reflection.AssemblyProductAttribute("DreamScannerApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DreamScannerApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
